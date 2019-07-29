89fb671f107dda6da7e0963f6eed2469d777d76b
Ariela Cohen <arielaades@gmail.com>
2019-07-29 19:38:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+  case
+  when integer.between?(2, 3)
+    return true
+  when integer <= 1
+    return false
+  else
+  (2..(integer-1)).each do |n|
+    if (integer % n) == 0
+      return false
+    end
+  end
+  end
+return true
+end
