a77a6a9e38de452c5d75a5f47ffdd7f381e7332a
pianoFF <fangfei1star@gmail.com>
2020-11-17 18:19:06 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+require 'benchmark'
+
+def prime?(int)
+  if int < 0
+    puts "Be sure to account for negative numbers!"
+    return false
+  elsif int == 0 || int == 1
+    return false
+  else 
+    range = 2..((int/2).floor)
+    for counter in range do 
+      if int % counter == 0
+        return false
+      end
+    end
+    return true 
+  end
+end
+
+puts Benchmark.measure{prime?(14832843489763459987232349984293476542348765123508761236467749183764545362719376454)} 
